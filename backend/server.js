e4437b55f2c6a5fea19b74af94994a58f0e63fed
LeeHungQNG <builequochung3004@gmail.com>
2024-10-14 19:47:12 +0700

    setup database moongoose and create models

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 // const express = require('express'); // in package.json => type: commonJS
 import express from 'express'; // in package.json => type: module
 import authRoutes from './routes/auth.route.js';
+import { ENV_VARS } from './config/envVars.js';
+import { connectDB } from './config/db.js';
+
 const app = express();
+
+const PORT = ENV_VARS.PORT;
 
 app.use('/api/v1/auth', authRoutes);
 
-app.listen(5000, () => {
-  console.log('Server started at localhost:5000');
+app.listen(PORT, () => {
+  console.log('Server started at http://localhost:' + PORT);
+  connectDB();
 });
