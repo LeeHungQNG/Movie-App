3df87606a82ba68d367fe0b72a3e89100dc2bc18
LeeHungQNG <builequochung3004@gmail.com>
2024-10-14 16:15:54 +0700

    setup routes for backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
 // const express = require('express'); // in package.json => type: commonJS
 import express from 'express'; // in package.json => type: module
-
+import authRoutes from './routes/auth.route.js';
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Server is running');
-});
+app.use('/api/v1/auth', authRoutes);
 
 app.listen(5000, () => {
   console.log('Server started at localhost:5000');
