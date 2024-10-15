8733f3d487f8ba8781a5aa19eff5f9161e5275da
LeeHungQNG <builequochung3004@gmail.com>
2024-10-15 21:02:23 +0700

    protect routes middleware

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,25 @@
 // const express = require('express'); // in package.json => type: commonJS
 import express from 'express'; // in package.json => type: module
+import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
 import movieRoutes from './routes/movie.route.js';
 import tvRoutes from './routes/tv.route.js';
 
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
+import { protectRoute } from './middleware/protectRoute.js';
+
 
 const app = express();
 
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); // allow use req.body in controller
+app.use(cookieParser());
+
 app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/movie', movieRoutes);
-app.use('/api/v1/tv',tvRoutes)
+app.use('/api/v1/movie', protectRoute, movieRoutes);
+app.use('/api/v1/tv', protectRoute, tvRoutes);
 
 app.listen(PORT, () => {
   console.log('Server started at http://localhost:' + PORT);
