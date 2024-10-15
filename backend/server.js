4d581d544a568a898ac868a0b557d62750ea228f
LeeHungQNG <builequochung3004@gmail.com>
2024-10-15 16:58:19 +0700

    fetch movie from api backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 // const express = require('express'); // in package.json => type: commonJS
 import express from 'express'; // in package.json => type: module
 import authRoutes from './routes/auth.route.js';
+import movieRoutes from './routes/movie.route.js';
+
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 
@@ -10,6 +12,7 @@ const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); // allow use req.body in controller
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/movie', movieRoutes);
 
 app.listen(PORT, () => {
   console.log('Server started at http://localhost:' + PORT);
