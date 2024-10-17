c7fa7e71bac2f31c179a8545721c8c324ad19e45
LeeHungQNG <builequochung3004@gmail.com>
2024-10-17 11:00:38 +0700

    build UI Auth screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
 import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import HomePage from './pages/home/HomePage';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+      {/* Outsite routes it means can see footer every page */}
+      <Footer />
+    </>
   );
 }
 
