bfd01636356587c5eefff50882f725ca87001be7
LeeHungQNG <builequochung3004@gmail.com>
2024-11-17 09:57:36 +0700

    handle search history

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import { useAuthStore } from './store/authUser';
 import { Loader } from 'lucide-react';
 import WatchPage from './pages/WatchPage';
 import SearchPage from './pages/SearchPage';
+import SearchHistoryPage from './pages/SearchHistoryPage';
 
 function App() {
   const { user, isCheckingAuth, authCheck } = useAuthStore();
@@ -38,6 +39,8 @@ function App() {
         <Route path="/watch/:id" element={user ? <WatchPage /> : <Navigate to={'/login'} />} />
 
         <Route path="/search" element={user ? <SearchPage /> : <Navigate to={'/login'} />} />
+
+        <Route path="/history" element={user ? <SearchHistoryPage /> : <Navigate to={'/login'} />} />
       </Routes>
       {/* Outsite routes it means can see footer every page */}
       <Footer />
