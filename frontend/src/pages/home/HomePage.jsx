4c39d2da721af177e795412db8c1ac38295e5484
LeeHungQNG <builequochung3004@gmail.com>
2024-11-17 14:06:09 +0700

    fix path signup page

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,6 +1,6 @@
 import HomeScreen from './HomeScreen.jsx';
 import AuthScreen from './AuthScreen.jsx';
-import { useAuthStore } from '../../store/authUser.jsx';
+import { useAuthStore } from '../../store/authUser.js';
 
 const HomePage = () => {
   const { user } = useAuthStore();
